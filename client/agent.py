ed6953bf8c522641f630de6e4b3b42f511854567
Carlos Buitrago <krlosm27@gmail.com>
2022-08-23 08:16:12 -0500

    planteamiento construccion agente

--- client/agent.py
+++ client/agent.py
@@ -1,11 +1,23 @@
 import subprocess
-import jason
-from subprocess import PIPE
+##import json
+##from subprocess import PIPE, run
+##import requests
 
-users = run (Who) ## Usuarios Conectados
-cpuinf = run (lscpu)  ## Informaciòn de CPU
-os = run (cat /etc/*release* | grep -E -i) ## Sistema operativo
-ver_os = run (cat /etc/ *release* | grep -E -i) ## Versiòn del sistema operativo
-procss = run (ps -ef | sed 's/\\/\/\//g' | grep -v sed) ## Listado de procesos activos
-date = run (date) ## Fecha
-ip address = () ## ip del sevidor 
+def monitoring():
+    prueba = subprocess.run(["ipconfig"])
+    ##users = subprocess.run(["who"]) ## Usuarios Conectados
+    ##cpuinf = subprocess.run(["lscpu"])  ## Informaciòn de CPU
+    ##os = subprocess.run(["cat /etc/*release* | grep -E -i "^NAME="]) ## Sistema operativo
+    ##ver_os = subprocess.run(["cat /etc/*release* | grep -E -i "^VERSION_ID"]) ## Versiòn del sistema operativo
+    ##procss = subprocess.run(["ps -ef | sed 's/\\/\/\//g' | grep -v sed"]) ## Listado de procesos activos
+    ##date = subprocess.run(["date"]) ## Fecha
+    return prueba
+
+print (monitoring())
+print(type(monitoring())) ## conocer tipo de dato
+
+##def monitoring_report(jsonresponse):
+
+    ##monitoring_report = {'users': users, 'cpuinfo': cpuinf}
+    ##resp = requests.post('192.168.1.11:8080/servers/monitoring', data=monitoring_report)
+    ##monitoring_report = {"sessions":users}
