e26be137994b0af70c58b24246cb92d21ccb8f44
Carlos Buitrago <krlosm27@gmail.com>
2022-08-23 17:57:05 -0500

    Eliminacion de archivos no usados

--- client/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-###users ='pepito'
-###cpuinf ='54545' 
-
-monitoring_report = {'users': 'Carlos', 'cpuinfo': '12345'}
-resp = requests.post('192.168.1.11:8080/servers/monitoring', data=monitoring_report)
