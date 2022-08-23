10b8966f5cdaa2637b6657b6b9fde518a2ac78c1
Carlos Buitrago <krlosm27@gmail.com>
2022-08-23 17:51:18 -0500

    Actualizacion codigo agente

--- client/agent.py
+++ client/agent.py
@@ -1,23 +1,27 @@
-import subprocess
-##import json
-##from subprocess import PIPE, run
-##import requests
+from subprocess import check_output
+import json
+import requests
 
-def monitoring():
-    prueba = subprocess.run(["ipconfig"])
-    ##users = subprocess.run(["who"]) ## Usuarios Conectados
-    ##cpuinf = subprocess.run(["lscpu"])  ## Informaciòn de CPU
-    ##os = subprocess.run(["cat /etc/*release* | grep -E -i "^NAME="]) ## Sistema operativo
-    ##ver_os = subprocess.run(["cat /etc/*release* | grep -E -i "^VERSION_ID"]) ## Versiòn del sistema operativo
-    ##procss = subprocess.run(["ps -ef | sed 's/\\/\/\//g' | grep -v sed"]) ## Listado de procesos activos
-    ##date = subprocess.run(["date"]) ## Fecha
-    return prueba
+sessions = check_output(["who"])
+cpuinf = check_output(["lscpu"])
+os = check_output(['cat /etc/*release* | grep -E -i PRETTY_NAME'], shell=True)
+ver_os = check_output(['cat /etc/*release* | grep -E -i ^VERSION_ID'], shell=True)
+process = check_output(['ps -ef | sed "s/u/o/g" | grep -v sed'], shell=True)
+#process = check_output(["ps -ef | sed 's/\\/\/\//g' | grep -v sed"])
+date = check_output(["date"])
+ip_server = check_output(["ip addr show"])
 
-print (monitoring())
-print(type(monitoring())) ## conocer tipo de dato
+monitoreo = [cpuinf, process, sessions, os, ver_os ] # lista de array de bits de los comandos ejecutados
+nombreArchivo = ip_server.decode("UTF-8") + date.decode("UTF-8") + ".json" # concatenado de nombre de archivo json
 
-##def monitoring_report(jsonresponse):
+with open(nombreArchivo, 'w') as archivoJson: # open (administra archivos) primer_arg = ruta_archivo segundo_arg = r - read, w-write
+    for dato in monitoreo:
+        datoDecodificado = dato.decode("UTF-8") # Decodifica cada array de bits en formato UTF-8 y lo guarda en nueva variable
+        archivoJson.writelines(datoDecodificado)
 
-    ##monitoring_report = {'users': users, 'cpuinfo': cpuinf}
-    ##resp = requests.post('192.168.1.11:8080/servers/monitoring', data=monitoring_report)
-    ##monitoring_report = {"sessions":users}
+
+ # res --> llevar a formato json
+def monitoring_report(nombreArchivo):
+    monitoring_report = {'users': sessions, 'cpuinfo': cpuinf,'operative_system': os, 'version_os': ver_os, 'process': process}
+    resp = requests.post('192.168.1.11:8080/servers/monitoring', data=monitoring_report)
+    
