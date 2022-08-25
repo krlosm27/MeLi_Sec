a542fef5ec75ef99a7d209dc6b83c67cdfb4f90f
Carlos Buitrago <krlosm27@gmail.com>
2022-08-24 19:22:13 -0500

    Actualizacion agente de monitoreo

--- client/agent.py
+++ client/agent.py
@@ -1,3 +1,4 @@
+import subprocess
 from subprocess import check_output
 import json
 import requests
@@ -7,21 +8,29 @@ cpuinf = check_output(["lscpu"])
 os = check_output(['cat /etc/*release* | grep -E -i PRETTY_NAME'], shell=True)
 ver_os = check_output(['cat /etc/*release* | grep -E -i ^VERSION_ID'], shell=True)
 process = check_output(['ps -ef | sed "s/u/o/g" | grep -v sed'], shell=True)
-#process = check_output(["ps -ef | sed 's/\\/\/\//g' | grep -v sed"])
+process = check_output(["ps -ef | sed 's/\\/\/\//g' | grep -v sed"])
 date = check_output(["date"])
 ip_server = check_output(["ip addr show"])
 
-monitoreo = [cpuinf, process, sessions, os, ver_os ] # lista de array de bits de los comandos ejecutados
-nombreArchivo = ip_server.decode("UTF-8") + date.decode("UTF-8") + ".json" # concatenado de nombre de archivo json
+nombreArchivo = ver_os.decode("UTF-8") + date.decode("UTF-8") + ".json" # concatenado de nombre de archivo json
 
-with open(nombreArchivo, 'w') as archivoJson: # open (administra archivos) primer_arg = ruta_archivo segundo_arg = r - read, w-write
-    for dato in monitoreo:
-        datoDecodificado = dato.decode("UTF-8") # Decodifica cada array de bits en formato UTF-8 y lo guarda en nueva variable
-        archivoJson.writelines(datoDecodificado)
+monitoring = {
+    'commands': {
+        'users': sessions.decode("UTF-8"),
+        'cpu_info': cpuinf.decode("UTF-8"),
+        'operative_system': os.decode("UTF-8"),
+        'version_os': ver_os.decode("UTF-8"),
+        'process': process.decode("UTF-8")
+    }
+}
+
+with open(nombreArchivo, 'w') as file:
+    json.dump(monitoring, file, indent=6)
+
+print (monitoring)
 
 
- # res --> llevar a formato json
 def monitoring_report(nombreArchivo):
     monitoring_report = {'users': sessions, 'cpuinfo': cpuinf,'operative_system': os, 'version_os': ver_os, 'process': process}
-    resp = requests.post('192.168.1.11:8080/servers/monitoring', data=monitoring_report)
+    resp = requests.post('192.168.1.11:8080/servers/monitoring', data=monitoring)
     
