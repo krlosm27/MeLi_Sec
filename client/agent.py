d6fd0dcda4d79a27c9f4da392341ccc9e39dafbe
Carlos Buitrago <krlosm27@gmail.com>
2022-08-27 16:29:40 -0500

    actualizacion agente envio request

--- client/agent.py
+++ client/agent.py
@@ -3,34 +3,31 @@ from subprocess import check_output
 import json
 import requests
 
-sessions = check_output(["who"])
-cpuinf = check_output(["lscpu"])
-os = check_output(['cat /etc/*release* | grep -E -i PRETTY_NAME'], shell=True)
-ver_os = check_output(['cat /etc/*release* | grep -E -i ^VERSION_ID'], shell=True)
-process = check_output(['ps -ef | sed "s/u/o/g" | grep -v sed'], shell=True)
-process = check_output(["ps -ef | sed 's/\\/\/\//g' | grep -v sed"])
-date = check_output(["date"])
-ip_server = check_output(["ip addr show"])
-
-nombreArchivo = ver_os.decode("UTF-8") + date.decode("UTF-8") + ".json" # concatenado de nombre de archivo json
+sessions = check_output(["who"]).decode("UTF-8")
+cpuinf = check_output(["lscpu"]).decode("UTF-8")
+os = check_output(['cat /etc/*release* | grep -E -i PRETTY_NAME'], shell=True).decode("UTF-8")
+ver_os = check_output(['cat /etc/*release* | grep -E -i ^VERSION_ID'], shell=True).decode("UTF-8")
+process = check_output(["ps -ef"], shell=True).decode("UTF-8")
+process = process.replace('\\', '/')
+date = check_output(["date"]).decode("UTF-8")
+ip_server = check_output(["ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk -F ' ' '{print $2}'"], shell=True).decode("UTF-8")
 
 monitoring = {
     'commands': {
-        'users': sessions.decode("UTF-8"),
-        'cpu_info': cpuinf.decode("UTF-8"),
-        'operative_system': os.decode("UTF-8"),
-        'version_os': ver_os.decode("UTF-8"),
-        'process': process.decode("UTF-8")
+        'users': sessions,
+        'cpu_info': cpuinf,
+        'operative_system': os,
+        'version_os': ver_os,
+        'process': process
     }
 }
 
-with open(nombreArchivo, 'w') as file:
-    json.dump(monitoring, file, indent=6)
 
-print (monitoring)
+def monitoring_report(serverURL, jsonString):
+    headers = {'Content-type': 'application/json'}
+    resp = requests.post(serverURL, data=json.dumps(jsonString), headers=headers)
+    print(resp.text)
 
 
-def monitoring_report(nombreArchivo):
-    monitoring_report = {'users': sessions, 'cpuinfo': cpuinf,'operative_system': os, 'version_os': ver_os, 'process': process}
-    resp = requests.post('192.168.1.11:8080/servers/monitoring', data=monitoring)
+monitoring_report('http://192.168.1.11:8080/servers/monitoring', monitoring)
     
