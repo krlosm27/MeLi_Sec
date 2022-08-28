261e5d19e0bbdf189938cb39cf75bd49046e39a8
Carlos Buitrago <krlosm27@gmail.com>
2022-08-27 20:48:15 -0500

    Consulta base de datos sin formato

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
 import pymongo
 from bson import json_util
+import datetime
 
 MONGODB_CONNECTION_STRING='mongodb://localhost:27017/'
 MONGODB_DATABASE_NAME='dbMeliSec'
@@ -21,10 +22,19 @@ def hello_world():
 def servers_monitoring():
     payload = request.json
     print(type(payload))
-    json_data = json.loads(json_util.dumps(payload))
-    print(type(json_data))
+    json_data = json.loads(json_util.dumps(payload)) 
     register = collection.insert_one(json_data)
-    return payload
+    return(payload)
 
+@app.route("/servers/getMonitoring", methods=['GET'] )
+def get_monitoring():
+    dictn = {}
+    retrieves = collection.find().limit(10)
+    for document in retrieves:
+        dictn.update(document)
+    jsonstring = json_util.dumps(dictn)
+    print(type(jsonstring))
+    return jsonify(jsonstring)
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
